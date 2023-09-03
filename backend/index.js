0231bf5c691ffa58b98c5995112a5089f6b3a3b3
Carlos Macbook Pro <carlosmac@Carloss-MacBook-Pro.local>
2023-09-03 04:26:22 -0300

    backend

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
@@ -12,40 +13,42 @@ const { addUserToQueue, findPairForUser } = require('./utils/userQueue');
 let waitingUsers = [];
 
 // Serve arquivos estáticos do diretório 'frontend'
-app.use(express.static('../frontend'));
+app.use(express.static('../frontend3/build'));
 
 app.use('/api', userRoutes);
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'frontend3', 'build', 'index.html'));
+});
 
 // Use o middleware CORS para permitir solicitações de diferentes origens
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/../frontend/index.html');
-});
-
-
-
 io.on('connection', (socket) => {
     console.log('Usuário conectado:', socket.id);
- 
     
     const pair = findPairForUser(socket.id);
   
     if (pair) {
         const roomId = `${socket.id}-${pair}`;
         socket.join(roomId);
-        io.sockets.sockets.get(pair).join(roomId);
-        
-        // Comunique o roomId aos sockets
-        socket.emit('pairFound', { partnerId: pair, roomId: roomId });
-        io.sockets.sockets.get(pair).emit('pairFound', { partnerId: socket.id, roomId: roomId });
-    }
     
-    
-    else {
+        const pairSocket = io.sockets.sockets.get(pair);
+        if (pairSocket) {
+            pairSocket.join(roomId);
+            
+            // Comunique o roomId aos sockets
+            socket.emit('pairFound', { partnerId: pair, roomId: roomId });
+            pairSocket.emit('pairFound', { partnerId: socket.id, roomId: roomId });
+        } else {
+            // Se o par não estiver mais conectado, adicione o usuário atual de volta à fila
+            addUserToQueue(socket.id);
+        }
+    } else {
         addUserToQueue(socket.id);
     }
+    
+
     socket.on('sendMessage', (data) => {
         console.log(`Mensagem recebida do usuário ${socket.id}: ${data.message}`);
     
@@ -54,21 +57,27 @@ io.on('connection', (socket) => {
     
         console.log(`Mensagem enviada para a sala ${data.roomId}`);
     });
-    
-    
-    
-    // Configuração básica do Socket.io
-    socket.on('receiveMessage', (data) => {
-        const chatBox = document.getElementById('chatBox');
-        chatBox.innerHTML += `<p>${data.sender}: ${data.message}</p>`;
-    });
+
     socket.on('disconnect', () => {
         console.log('Usuário desconectado:', socket.id);
         // Remova o usuário da fila se ele se desconectar antes de encontrar um par
         waitingUsers = waitingUsers.filter(u => u !== socket.id);
     });
+
+   
+    // Listener para desconexão do usuário
+socket.on('userDisconnected', (data) => {
+    // Notificar o parceiro de que o usuário se desconectou
+    const partnerSocketId = data.roomId.replace(socket.id, '').replace('-', '');
+    const partnerSocket = io.sockets.sockets.get(partnerSocketId);
+    if (partnerSocket) {
+        partnerSocket.emit('forceDisconnect');
+        partnerSocket.disconnect();
+    }
 });
 
+
+});
 
 const PORT = 3000;
 server.listen(PORT, () => {
